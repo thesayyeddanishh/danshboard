59c36bfd901eed37acb8c18bfb3195c9201b1b6f
Danish Sayyed <106588484+thesayyeddanishh@users.noreply.github.com>
2026-06-14 15:25:23 +0530

    Update 1_BATTERS.py\

--- pages/1_BATTERS.py
+++ pages/1_BATTERS.py
@@ -2,6 +2,9 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.patches as patches
+import matplotlib.cm as cm
+import matplotlib.colors
 import sys
 import os
 
